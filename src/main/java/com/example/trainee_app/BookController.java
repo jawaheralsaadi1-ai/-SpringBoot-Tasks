f2e72d1f48b82b7b9bdf28626f1d1a7856c09a42
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-03 17:26:38 +0400

    ad: endpoint /add-book to BookController

--- src/main/java/com/example/trainee_app/BookController.java
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -1,5 +1,7 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,5 +10,11 @@ import java.util.List;
 public class BookController {
 
     private static List<Book> bookShelf = new ArrayList<Book>();
+    @GetMapping("/add-book")
+    public String addBook(@RequestParam int id, @RequestParam String name) {
+        Book book = new Book(id, name);
+        bookShelf.add(book);
+        return "Book added successfully!";
+    }
 
 }
