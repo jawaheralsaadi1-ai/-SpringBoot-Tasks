74461a36f77fe8b42709b3a710d1a5b6820deca7
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-03 17:18:34 +0400

    add : BookController class

--- src/main/java/com/example/trainee_app/Book.java
+++ src/main/java/com/example/trainee_app/Book.java
@@ -7,7 +7,7 @@ public class Book {
         private int id;
         private String name;
 
-        //  constructor
+        //  constructorg
         public Book(int id, String name) {
             this.id = id;
             this.name = name;
---
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -0,0 +1,12 @@
+package com.example.trainee_app;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private static List<Book> bookShelf = new ArrayList<Book>();
+
+}
