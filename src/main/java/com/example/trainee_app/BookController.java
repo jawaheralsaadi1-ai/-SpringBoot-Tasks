6ae77b3519246deacd9f328c3c7c7baee4c43312
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-04 13:21:52 +0400

    add:create LibraryController class

--- src/main/java/com/example/trainee_app/BookController.java
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -1,3 +1,4 @@
+/*
 package com.example.trainee_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,3 +51,4 @@ public class BookController {
         return "Sorry, that book ID is not available.";
     }
 }
+*/
---
+++ src/main/java/com/example/trainee_app/LibraryController.java
@@ -0,0 +1,11 @@
+package com.example.trainee_app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LibraryController {
+    private static List<Author> authorList = new ArrayList<>();
+    private static List<Book> bookList = new ArrayList<>();
+
+}
