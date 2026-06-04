13c9b96da1f950c1a07f76d10c18c309541db1ba
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-04 13:24:29 +0400

    add: endpoint /addAuthor to LibController

--- src/main/java/com/example/trainee_app/LibraryController.java
+++ src/main/java/com/example/trainee_app/LibraryController.java
@@ -1,5 +1,8 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,5 +10,12 @@ import java.util.List;
 public class LibraryController {
     private static List<Author> authorList = new ArrayList<>();
     private static List<Book> bookList = new ArrayList<>();
+
+    @GetMapping("/addAuthor")
+    public String addAuthor(@RequestParam int id, @RequestParam String name, @RequestParam String biography) {
+        authorList.add(new Author(id, name, biography));
+        return "Author " + name + " added successfully!";
+
+}
 
 }
