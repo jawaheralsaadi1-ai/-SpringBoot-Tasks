7d8985df40133e36443f766c086587855e02ca35
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-04 13:48:19 +0400

    edit:LibController class

--- src/main/java/com/example/trainee_app/LibraryController.java
+++ src/main/java/com/example/trainee_app/LibraryController.java
@@ -2,11 +2,12 @@ package com.example.trainee_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@RestController
 public class LibraryController {
     private static List<Author> authorList = new ArrayList<>();
     private static List<Book> bookList = new ArrayList<>();
