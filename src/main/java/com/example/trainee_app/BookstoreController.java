348027f73a13bcb00476b981d7db572e50033fe8
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-07 11:51:26 +0400

    add: create BookstoreController class.

---
+++ src/main/java/com/example/trainee_app/BookstoreController.java
@@ -0,0 +1,35 @@
+package com.example.trainee_app;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+public class BookstoreController {
+
+    // Initialize a static in-memory List to store the book catalog
+    private static final List<InventoryBook> catalog = new ArrayList<>();
+
+   // Mapping : GET endpoint
+    @GetMapping("/add-book")//   //Web Path: /add-book
+    public String addBook(
+            @RequestParam int id,
+            @RequestParam String title,
+            @RequestParam double price,
+            @RequestParam int stock) {
+
+        //  Create a new object using the constructor
+        InventoryBook newBook = new InventoryBook(id, title, price, stock);
+
+        //  Add to list
+        catalog.add(newBook);
+
+        //  Return a confirmation message.
+        return "Success: The book '" + title + "' (ID: " + id + ") has been successfully added to the bookstore's catalog.";
+    }
+}
