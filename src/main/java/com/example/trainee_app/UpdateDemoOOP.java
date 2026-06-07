af60615a8fc3019b27a73c577328a81ef84b27be
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-07 16:42:51 +0400

    add(UpdateDemoOOP): Display Student details using getters.

--- src/main/java/com/example/trainee_app/UpdateDemoOOP.java
+++ src/main/java/com/example/trainee_app/UpdateDemoOOP.java
@@ -6,6 +6,13 @@ public class UpdateDemoOOP {
 // Create a Student object with initial values
         Student student = new Student(101, "Ahmed", "A");
 
+        //Display original student details using getters
+        System.out.println("--- Initial Student Data ---");
+        System.out.println("Student ID   : " + student.getStudentId());
+        System.out.println("Student Name : " + student.getStudentName());
+        System.out.println("Grade        : " + student.getGrade());
+        System.out.println("-----------------------------");
+
 
     }
 }
