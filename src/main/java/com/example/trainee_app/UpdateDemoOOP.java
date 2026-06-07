745a000baa72e60fe54660b13ff97197d275f50b
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-07 16:39:36 +0400

    add(UpdateDemoOOP):Student object with initial values.

--- src/main/java/com/example/trainee_app/UpdateDemoOOP.java
+++ src/main/java/com/example/trainee_app/UpdateDemoOOP.java
@@ -3,6 +3,9 @@ package com.example.trainee_app;
 public class UpdateDemoOOP {
 
     public static void main(String[] args) {
+// Create a Student object with initial values
+        Student student = new Student(101, "Ahmed", "A");
 
-           }
+
+    }
 }
