ec5c830302faf548c3a80d502aec85e1c0ead7d3
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-07 17:16:11 +0400

    add(StudentController) : PUT /updateStudent endpoint.

--- src/main/java/com/example/trainee_app/StudentController.java
+++ src/main/java/com/example/trainee_app/StudentController.java
@@ -1,10 +1,22 @@
 package com.example.trainee_app;
 
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
-    //  Create a sample Student object — represents existing stored data
+    //  Create Student object
     private Student student = new Student(101, "Ahmed", "A");
+
+    //  PUT endpoint at /update-student
+    @PutMapping("/updateStudent")
+    public void updateStudent(@RequestParam String name) {
+
+        // Retrieve current value before update
+        String previousName = student.getStudentName();
+
+    }
 }
