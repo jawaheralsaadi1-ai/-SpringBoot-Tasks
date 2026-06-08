5303c7c58d8b1f60a1461d6064c2d0ce8764fd0c
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-08 16:48:59 +0400

    add(UpdateEmailDemo) : declaring variables with sample values.

--- src/main/java/com/example/trainee_app/UpdateEmailDemo.java
+++ src/main/java/com/example/trainee_app/UpdateEmailDemo.java
@@ -1,4 +1,10 @@
 package com.example.trainee_app;
 
 public class UpdateEmailDemo {
+    //Declaring Variables
+    private String currentEmailAddress = "ahmed@example.com";
+    private String newEmailAddress = "ali@example.com";
+    boolean isUpdated = false;
+
+
 }
