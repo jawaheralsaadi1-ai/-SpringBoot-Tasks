20afcc19da44ba00b9705f8dfe221701290e81b3
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 00:21:50 +0400

    add: create Order class with private fields

---
+++ src/main/java/com/example/trainee_app/Order.java
@@ -0,0 +1,9 @@
+package com.example.trainee_app;
+
+public class Order {
+
+    //Declaring Variables
+    private int    orderId;
+    private String shippingAddress;
+    private String orderStatus;
+}
