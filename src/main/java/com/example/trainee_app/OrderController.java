0fd7a12a1492338b1f138ed76a8171a6011ae59a
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 00:29:53 +0400

    add(OrderController): add /updateOrder/{orderId} PUT endpoint.

--- src/main/java/com/example/trainee_app/OrderController.java
+++ src/main/java/com/example/trainee_app/OrderController.java
@@ -1,5 +1,8 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -14,5 +17,13 @@ public class OrderController {
         orderMap.put(101, new Order(101, "Muscat", "Pending"));
         orderMap.put(102, new Order(102, "Sohar", "Pending"));
         orderMap.put(103, new Order(103, "Salalah", "Shipped"));
+    }
+    // PUT endpoint — updates fields
+    @PutMapping("/updateOrder/{orderId}")
+    public String updateOrder(@PathVariable int orderId,
+                              @RequestParam String address,
+                              @RequestParam String status) {
+
+        return address;
     }
 }
