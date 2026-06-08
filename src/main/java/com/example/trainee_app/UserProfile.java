8f849fb0a0673b1269ffd305e4b138e40aae5092
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-08 17:35:08 +0400

    add: create Userprofile class with private fields.

---
+++ src/main/java/com/example/trainee_app/UserProfile.java
@@ -0,0 +1,12 @@
+package com.example.trainee_app;
+
+public class UserProfile {
+
+    //Declaring variables
+    // private - to enforce encapsulation and prevent direct modification.
+    private int userID;
+    private String phoneNumber;
+    private String address;
+
+
+}
