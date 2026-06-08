b1f393941084de5f4b2945e6aef111d23806caae
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 00:27:57 +0400

    add(OrderController): preload sample orders into HashMap.

--- src/main/java/com/example/trainee_app/OrderController.java
+++ src/main/java/com/example/trainee_app/OrderController.java
@@ -9,4 +9,10 @@ public class OrderController {
     //  storage a  database using HashMap
     private HashMap<Integer, Order> orderMap = new HashMap<>();
 
+    // Preload sample data
+    public OrderController() {
+        orderMap.put(101, new Order(101, "Muscat", "Pending"));
+        orderMap.put(102, new Order(102, "Sohar", "Pending"));
+        orderMap.put(103, new Order(103, "Salalah", "Shipped"));
+    }
 }
