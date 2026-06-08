eaf610a28001ac495df1736cb074ad3310853fa9
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-08 23:43:11 +0400

    add(ProductController): add /updateStock{Id} PUT endpoint.

--- src/main/java/com/example/trainee_app/ProductController.java
+++ src/main/java/com/example/trainee_app/ProductController.java
@@ -1,5 +1,8 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,10 +14,16 @@ public class ProductController {
 
     // Preload sample product data
     public ProductController() {
-        productMap.put(1, new Product(1, "Laptop",  10));
+        productMap.put(1, new Product(1, "Laptop", 10));
         productMap.put(2, new Product(2, "Monitor", 25));
         productMap.put(3, new Product(3, "Keyboard", 50));
     }
 
-
+    // PUT endpoint
+    @PutMapping("/updateStock{Id}")
+    //Configure the method
+    public String updateStock(@PathVariable int id,
+                              @RequestParam int quantity) {
+        return "";
+    }
 }
