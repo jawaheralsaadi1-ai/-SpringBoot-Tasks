2d6dd8c7f057756169da4444a7a4edf9b678073f
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-08 23:29:30 +0400

    add(Product): add constructor for all fields.

--- src/main/java/com/example/trainee_app/Product.java
+++ src/main/java/com/example/trainee_app/Product.java
@@ -7,4 +7,11 @@ public class Product {
     private String name;
     private int    stockQuantity;
 
+   // constructor to initialize all fields
+    public Product(int id, String name, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+    }
+
 }
