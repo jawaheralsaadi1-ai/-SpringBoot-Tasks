9ab14e090082ccf1d66b9b9f42128c3508eb3ba2
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 20:30:23 +0400

    add(EventRegistration): add constructor for all fields.

--- src/main/java/com/example/trainee_app/EventRegistration.java
+++ src/main/java/com/example/trainee_app/EventRegistration.java
@@ -5,4 +5,10 @@ public class EventRegistration {
     // declaring variables
     private int    registrationId;
     private String attendeeName;
+
+    //constructor to initialize all fields
+    public EventRegistration(int registrationId, String attendeeName) {
+        this.registrationId = registrationId;
+        this.attendeeName = attendeeName;
+    }
 }
