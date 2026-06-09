b832d43ca97066b0ce3d6691d36cc88bc83534ee
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 19:42:50 +0400

    add: create DeleteMemberDemo class.

--- src/main/java/com/example/trainee_app/DeleteMemberDemo.java
+++ src/main/java/com/example/trainee_app/DeleteMemberDemo.java
@@ -1,4 +1,32 @@
 package com.example.trainee_app;
 
 public class DeleteMemberDemo {
-}
+    public static void main(String[] args) {
+
+        LibraryManager manager1 = new LibraryManager();
+        manager1.initializeMembers();
+
+        System.out.println("\n--- Initial Member List ---");
+        manager1.displayMembers();
+        System.out.println("---------------------------");
+
+        String targetId1 = "M102";
+        System.out.println("\nTarget Member ID : " + targetId1);
+
+         boolean isDeleted1 = manager1.deleteMemberById(targetId1);
+
+        if (isDeleted1) {
+            System.out.println("Member \"" + targetId1 + "\" deleted successfully.");
+        } else {
+            System.out.println("Member not found. No deletion performed.");
+        }
+
+        System.out.println("\n--- Updated Member List ---");
+        manager1.displayMembers();
+        System.out.println("---------------------------");
+
+
+
+    }
+
+    }
