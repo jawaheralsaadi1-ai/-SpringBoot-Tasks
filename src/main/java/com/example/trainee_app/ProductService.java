491cbb0a414da692cf996ee49694b20c8212b5c2
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 19:56:36 +0400

    add: create ProductService class with HashMap storage.

---
+++ src/main/java/com/example/trainee_app/ProductService.java
@@ -0,0 +1,8 @@
+package com.example.trainee_app;
+
+import java.util.HashMap;
+
+public class ProductService {
+    // Private collection — simulates database using HashMap
+    private HashMap<Integer, Product> productMap = new HashMap<>();
+}
