6bc421c9efe7a87f31eec76abd9bad226e9d3b36
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 19:58:44 +0400

    add(ProductService): add loadSampleProducts method with sample data.

--- src/main/java/com/example/trainee_app/ProductService.java
+++ src/main/java/com/example/trainee_app/ProductService.java
@@ -5,4 +5,15 @@ import java.util.HashMap;
 public class ProductService {
     // Private collection — simulates database using HashMap
     private HashMap<Integer, Product> productMap = new HashMap<>();
+
+    // Initialize sample data
+    public ProductService() {
+        loadSampleProducts();
+    }
+    // Load at least 3 sample products into the map
+    private void loadSampleProducts() {
+            productMap.put(1, new Product(1, "Laptop", 500));
+            productMap.put(2, new Product(2, "Phone",  300));
+            productMap.put(3, new Product(3, "Tablet", 200));
+    }
 }
