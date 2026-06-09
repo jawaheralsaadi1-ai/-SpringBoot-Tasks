db5fef58602914bbd552ca98cd2c9440cc2fd4ac
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 20:35:18 +0400

    add(RegistrationService): initialize data.

--- src/main/java/com/example/trainee_app/RegistrationService.java
+++ src/main/java/com/example/trainee_app/RegistrationService.java
@@ -11,4 +11,14 @@ import java.util.Map;
 
         // declaring collection — simulates database using HashMap
         private Map<Integer, EventRegistration> registrations = new HashMap<>();
-}
+
+
+        // Initialize sample data
+        public RegistrationService() {
+            registrations.put(101, new EventRegistration(101, "Ahmed"));
+            registrations.put(102, new EventRegistration(102, "Sara"));
+            registrations.put(103, new EventRegistration(103, "John"));
+            registrations.put(104, new EventRegistration(104, "Fatima"));
+            registrations.put(105, new EventRegistration(105, "Omar"));
+        }
+    }
