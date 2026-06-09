7d7d1a8746d200a61f6266274286def9be6da34e
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 18:59:04 +0400

    add(LibraryMember): add constructor for all fields.

--- src/main/java/com/example/trainee_app/LibraryMember.java
+++ src/main/java/com/example/trainee_app/LibraryMember.java
@@ -1,8 +1,15 @@
 package com.example.trainee_app;
 
 public class LibraryMember {
+    // Declaring Variables
     private String memberId;
     private String fullName;
     private String membersshipType;
 
+// constructor
+    public LibraryMember(String memberId, String fullName, String membersshipType) {
+        this.memberId = memberId;
+        this.fullName = fullName;
+        this.membersshipType = membersshipType;
+    }
 }
