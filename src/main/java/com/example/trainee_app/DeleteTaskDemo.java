bed8bd256ffcf1583fa92677d93b6c19f4e06286
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 18:41:17 +0400

    add(DeleteTaskDemo): Declare and initialize a list.

--- src/main/java/com/example/trainee_app/DeleteTaskDemo.java
+++ src/main/java/com/example/trainee_app/DeleteTaskDemo.java
@@ -1,4 +1,14 @@
 package com.example.trainee_app;
 
+import java.util.ArrayList;
+
 public class DeleteTaskDemo {
+    public static void main(String[] args) {
+
+    // Initialize task list with sample data
+    ArrayList<String> tasks1 = new ArrayList<>();
+                    tasks1.add("Buy groceries");
+                    tasks1.add("Call doctor");
+                    tasks1.add("Finish report");
+ }
 }
