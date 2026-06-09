27ec28e93eefb0de261332552febf502a0ed339e
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-09 19:05:55 +0400

    add(LibraryManager): add initialize Members method.

--- src/main/java/com/example/trainee_app/LibraryManager.java
+++ src/main/java/com/example/trainee_app/LibraryManager.java
@@ -7,5 +7,10 @@ public class LibraryManager {
     //Declaring a private collection
     private ArrayList<LibraryMember> members = new ArrayList<>();
 
-
+    // Initialize collection with sample members
+    public void initializeMembers() {
+        members.add(new LibraryMember("M101", "Ahmed", "Standard"));
+        members.add(new LibraryMember("M102", "Sara", "Premium"));
+        members.add(new LibraryMember("M103", "John", "Standard"));
+    }
 }
