e56300f74f850a9b1108fd507b5dce1302a1e374
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 11:47:50 +0400

    add(AddCampaignDemo): display existing campaign before POST operation.

--- src/main/java/com/example/trainee_app/AddCampaignDemo.java
+++ src/main/java/com/example/trainee_app/AddCampaignDemo.java
@@ -1,4 +1,13 @@
 package com.example.trainee_app;
 
 public class AddCampaignDemo {
+    public static void main(String[] args) {
+    CampaignManager manager1 = new CampaignManager();
+
+    // Display existing campaigns before POST operation
+                    System.out.println("\n--- Existing Campaigns ---");
+                    manager1.displayCampaigns();
+                    System.out.println("--------------------------");
+
+}
 }
