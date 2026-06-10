1a85dffc48f7ebfbdc7688570c74f1aadd1b6f6e
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 10:31:26 +0400

    add(Vehicle):  Getter methods for all fields.

--- src/main/java/com/example/trainee_app/Vehicle.java
+++ src/main/java/com/example/trainee_app/Vehicle.java
@@ -6,10 +6,24 @@ public class Vehicle {
     private String vehicleModel;
     private String rentalPricePerDay;
 
-    // Constructor - Initialize all fields.
+    // Constructor - Initialize all fields when object is created
     public Vehicle(int vehicleId, String vehicleModel, String rentalPricePerDay) {
         this.vehicleId = vehicleId;
         this.vehicleModel = vehicleModel;
         this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    //Getter methods — safe read access from outside the class
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public String getRentalPricePerDay() {
+        return rentalPricePerDay;
     }
 }
