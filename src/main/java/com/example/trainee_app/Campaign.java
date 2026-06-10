402efc887a4afaa00d6338d445ac3866805a7130
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 11:32:51 +0400

    add: create Campaign class with private fields

---
+++ src/main/java/com/example/trainee_app/Campaign.java
@@ -0,0 +1,9 @@
+package com.example.trainee_app;
+
+public class Campaign {
+//Declaring variables
+    private String campaignId;
+    private String campaignName;
+    private String platform;
+    private double budget;
+}
