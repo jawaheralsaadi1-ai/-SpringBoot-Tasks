6590705f6a33c93fd5357ed89cafcab9ef908f9b
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 10:29:41 +0400

    add(Vehicle):  Constructor for all fields.

--- src/main/java/com/example/trainee_app/Vehicle.java
+++ src/main/java/com/example/trainee_app/Vehicle.java
@@ -6,5 +6,10 @@ public class Vehicle {
     private String vehicleModel;
     private String rentalPricePerDay;
 
-
+    // Constructor - Initialize all fields.
+    public Vehicle(int vehicleId, String vehicleModel, String rentalPricePerDay) {
+        this.vehicleId = vehicleId;
+        this.vehicleModel = vehicleModel;
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
 }
