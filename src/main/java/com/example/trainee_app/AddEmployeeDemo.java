ae65a5916f5ad75a47a63b6da5a55ccf7be44232
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 10:15:34 +0400

    add(AddEmployeeDemo):  add duplicate validation using loop.

--- src/main/java/com/example/trainee_app/AddEmployeeDemo.java
+++ src/main/java/com/example/trainee_app/AddEmployeeDemo.java
@@ -1,5 +1,6 @@
 package com.example.trainee_app;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
 public class AddEmployeeDemo {
@@ -8,9 +9,32 @@ public class AddEmployeeDemo {
 //
         // Initialize employee list with sample data
         ArrayList<String[]> employees1 = new ArrayList<>();//Create  collection to store employee records.
-        employees1.add(new String[]{"E101", "Sara",   "HR"});
-        employees1.add(new String[]{"E102", "Ahmed",  "Finance"});
-        employees1.add(new String[]{"E103", "John",   "Marketing"});
-    }
+        employees1.add(new String[]{"E101", "Sara", "HR"});
+        employees1.add(new String[]{"E102", "Ahmed", "Finance"});
+        employees1.add(new String[]{"E103", "John", "Marketing"});
 
+
+        // Display existing employees before POST operation
+        System.out.println("\n--- Existing Employees ---");
+        for (String[] emp : employees1) {
+            System.out.println("  - " + emp[0] + " → " + emp[1] + " → " + emp[2]);
+        }
+        System.out.println("--------------------------");
+
+        // New employee data to add
+        String[] newEmployee1 = {"E104", "Ali", "IT"};
+        System.out.println("\nNew Employee : "
+                + newEmployee1[0] + " → "
+                + newEmployee1[1] + " → "
+                + newEmployee1[2]);
+
+        // Validation — check if employee ID already exists
+        boolean isDuplicate1 = false;
+        for (String[] emp : employees1) {
+            if (emp[0].equalsIgnoreCase(newEmployee1[0])) {
+                isDuplicate1 = true;
+
+            }
+        }
+    }
 }
