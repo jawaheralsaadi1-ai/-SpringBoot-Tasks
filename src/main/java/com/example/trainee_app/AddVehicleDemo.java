558febe55f3afca1ee0189ab0c98416e354fa806
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 11:20:54 +0400

    add: create AddVehicleDemo.

---
+++ src/main/java/com/example/trainee_app/AddVehicleDemo.java
@@ -0,0 +1,9 @@
+package com.example.trainee_app;
+
+public class AddVehicleDemo {
+    public static void main(String[] args) {
+        VehicleManager manager1 = new VehicleManager();
+
+
+    }
+}
--- src/main/java/com/example/trainee_app/VehicleManager.java
+++ src/main/java/com/example/trainee_app/VehicleManager.java
@@ -49,6 +49,8 @@ public class VehicleManager {
                         + " → " + vehicle.getVehicleModel()
                         + " → "  + vehicle.getRentalPricePerDay() + " OMR/day");
             }
+
+
         }
     }
 
