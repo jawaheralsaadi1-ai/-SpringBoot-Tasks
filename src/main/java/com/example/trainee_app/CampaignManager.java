a1c7953456ac47ad139c33a884547b9d686aef92
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 11:38:49 +0400

    add: create CampaignManager class with ArrayList collection.

---
+++ src/main/java/com/example/trainee_app/CampaignManager.java
@@ -0,0 +1,10 @@
+package com.example.trainee_app;
+
+import java.util.ArrayList;
+
+//--This class will manage all marketing campaign operations--
+public class CampaignManager {
+
+    // create collection to store data
+    private ArrayList<Campaign> campaigns = new ArrayList<>();
+}
