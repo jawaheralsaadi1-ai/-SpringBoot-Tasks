c35234360e7f743f5fff4e9fe6cf1ad7e58a6ec4
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 10:34:15 +0400

    add: create VehicleManager class with ArrayList collection.

---
+++ src/main/java/com/example/trainee_app/VehicleManager.java
@@ -0,0 +1,12 @@
+package com.example.trainee_app;
+
+import java.util.ArrayList;
+
+
+//--This class will manage all vehicle-related operations--
+public class VehicleManager {
+
+    //  Create a Vehicle Collection
+    private ArrayList<Vehicle> vehicles = new ArrayList<>();
+
+}
