0dcb0c6c637e90684f23d43a45e14353c285269e
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 10:47:48 +0400

    add(VehicleManager): initialize collection with sample vehicles.

--- src/main/java/com/example/trainee_app/VehicleManager.java
+++ src/main/java/com/example/trainee_app/VehicleManager.java
@@ -10,5 +10,11 @@ public class VehicleManager {
     //  Create a Vehicle Collection
     private ArrayList<Vehicle> vehicles = new ArrayList<>();
 
+    // Initialize collection with sample vehicles
+    public VehicleManager() {
+        vehicles.add(new Vehicle("V101", "Toyota Corolla",   20));
+        vehicles.add(new Vehicle("V102", "Nissan Sunny",     18));
+        vehicles.add(new Vehicle("V103", "Hyundai Elantra",  22));
+    }
 
 }
