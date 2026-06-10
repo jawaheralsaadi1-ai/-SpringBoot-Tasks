17ac91671049c7df7bea23c6a9d9d128c76e59ab
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-10 11:23:17 +0400

    add(AddVehicleDemo): display existing vehicle before POST operation.

--- src/main/java/com/example/trainee_app/AddVehicleDemo.java
+++ src/main/java/com/example/trainee_app/AddVehicleDemo.java
@@ -4,6 +4,11 @@ public class AddVehicleDemo {
     public static void main(String[] args) {
         VehicleManager manager1 = new VehicleManager();
 
+// Display existing vehicles before POST operation
+        System.out.println("\n--- Existing Vehicles ---");
+        manager1.displayVehicles();
+        System.out.println("-------------------------");
+
 
     }
 }
