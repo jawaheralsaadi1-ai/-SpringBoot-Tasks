0f070086be503378d6083a055cc61edefd4fac74
jawaheralsaadi1-ai <jawaheralsaadi1@gmail.com>
2026-06-14 10:30:07 +0400

    add: create CampaignRepository extending JpaRepository.

---
+++ src/main/java/com/example/trainee_app/repository/CampaignRepository.java
@@ -0,0 +1,9 @@
+﻿package com.example.trainee_app.repository;
+
+import com.example.trainee_app.entities.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+}
