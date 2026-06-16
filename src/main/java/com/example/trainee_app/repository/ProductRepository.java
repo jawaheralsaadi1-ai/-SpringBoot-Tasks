3ad040449fbd9d1feb30c35c0684a747567b84d2
jawaheralsaadi1-ai <jawaheralsaadi1@gmail.com>
2026-06-16 13:42:14 +0400

    add(ProductRepository): add Custom query method.

--- src/main/java/com/example/trainee_app/repository/ProductRepository.java
+++ src/main/java/com/example/trainee_app/repository/ProductRepository.java
@@ -1,4 +1,4 @@
-﻿package com.example.trainee_app.repository;
+package com.example.trainee_app.repository;
 
 import com.example.trainee_app.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    // Custom query: check if a product name already exists
+    boolean existsByProductName(String productName);
 }
